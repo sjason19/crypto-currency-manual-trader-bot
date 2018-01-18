4e66fe16e07028d62e2abd766237c82594bd4474
Jason Smith <jasonsmith@one45.com>
2018-01-17 23:56:51 -0800

    Setup draft RSI algorithm

--- lib/indicators.php
+++ lib/indicators.php
@@ -8,19 +8,69 @@
 *
 * Function to calculate the RSI (Relative Strength Index)
 *
-* @param string $symbol    The name of the symbol (see `/symbols`).
+* @param string $data    Historical close pricing data
 * @return mixed
 */
-function calculate_rsi() {
+function calculate_rsi($data) {
+  $closing_prices = [];
+  $upward_movement = [];
+  $downward_movement = [];
+  $avg_upward_movement = [];
+  $avg_downward_movement = [];
+  $relative_strength = [];
+
+  // Append all closing_price data to an array in the past 14 days
+  for($i=23; i>0; i--)
+  {
+    $j = 0;
+    $closing_prices[$j] = $data["Data"][$j]["close"];
+    $j++;
+  }
+
+  // Calculate Upward/Downward Movement
+  for($i=1; i<=23; i++)
+  {
+    $prev = $i--;
+
+    if ($closing_prices[$i] > $closing_prices[$prev])
+    {
+      $upward_movement[$i] = $closing_prices[$i] - $closing_prices[$prev];
+      $downward_movement[$i] = 0
+    }
+    else
+    {
+      $downward_movement[$i] = $closing_prices[$prev] - $closing_prices[$i];
+      $upward_movement[$i] = 0
+    }
+  }
+
+  // Array's containing first 14 elements of avg movement
+  $um_14 = array_slice($upward_movement, 0, 14);
+  $dm_14 = array_slice($downward_movement, 0, 14);
+
+  $avg_upward_movement[0] = array_sum($um_14) / 14;
+  $avg_downward_movement[0] = array_sum($um_14) / 14;
+  for($i=1; i<10; i++)
+  {
+    $prev = $i--;
+    $avg_upward_movement[$i] = ($avg_upward_movement[$prev] * (14 - 1) + $upward_movement[$i + 15]) / 14
+    $avg_downward_movement[$i] = ($avg_downward_movement[$prev] * (14 - 1) + $downward_movement[$i + 15]) / 14
+  }
+
+
+  for($i=0; i<9; i++)
+  {
+    $relative_strength[$i] = $avg_upward_movement / $avg_downward_movement;
+  }
+
   return 49;
 }
 
 /**
-* Calculate RSI
+* Calculate Moving average convergence divergence (MACD)
 *
-* Function to calculate the RSI (Relative Strength Index)
+* Function to calculate the Moving average convergence divergence (MACD)
 *
-* @param string $symbol    The name of the symbol (see `/symbols`).
 * @return mixed
 */
 function calculate_macd() {
--- status.php
+++ status.php
@@ -2,22 +2,25 @@
 include_once('./bitfinex.php');
 include_once('./lib/indicators.php');
 
-$qry_str = "?fsym=BTC&tsyms=USD&ts=1452680400";
+$qry_str = "?fsym=BTC&tsym=USD&limit=23&e=CCCAGG";
 $ch = curl_init();
 
 // Set query data here with the URL
-curl_setopt($ch, CURLOPT_URL, 'https://min-api.cryptocompare.com/data/pricehistorical' . $qry_str);
+curl_setopt($ch, CURLOPT_URL, 'https://min-api.cryptocompare.com/data/histoday' . $qry_str);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_TIMEOUT, 3);
 curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:7.0.1) Gecko/20100101 Firefox/7.0.1');
-$content = json_decode(trim(curl_exec($ch)));
+$content = trim(curl_exec($ch));
 curl_close($ch);
 
-var_dump($content);
+$content = json_decode($content, true);
+var_dump($content["Data"][0]["close"]);
+
+$rsi = calculate_rsi($content);
 
 
-if (calculate_rsi() < 50 || calculate_macd() > 0)
+if ($rsi < 50 || calculate_macd() > 0)
 {
   $message = "Good time to sell: " . $coin . "\n";
 }
