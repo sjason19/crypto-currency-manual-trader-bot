de02da50640614eab3081f120f10483582c3c217
Patrick Lin <patricklin@alumni.ubc.ca>
2018-01-21 10:10:50 -0800

    clean up obv

--- lib/indicators.php
+++ lib/indicators.php
@@ -132,12 +132,10 @@ function calculate_obv($data) {
     global $CURRENT_CLOSE;
     if ($CURRENT_CLOSE == 0) {
       $CURRENT_CLOSE = $item["close"];
-      $carry = 0;
-    }
-    if ($item["close"] == $CURRENT_CLOSE) {
-      $CURRENT_CLOSE = $item["close"];
+      $carry -= $item["volumefrom"];
       return $carry;
     }
+    if ($item["close"] == $CURRENT_CLOSE) return $carry;
     ($item["close"] > $CURRENT_CLOSE) ? $carry += $item["volumefrom"] : $carry -= $item["volumefrom"];
     $CURRENT_CLOSE = $item["close"];
     return $carry;
